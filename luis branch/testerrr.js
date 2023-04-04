277205552f0987fe3a41da48597ad8dd0d7cc12b
luisvillarreal <secretangel21@outlook.com>
2023-04-03 20:50:31 -0600

    added second page and some js

--- luis branch/testerrr.js
+++ luis branch/testerrr.js
@@ -1,9 +1,11 @@
 var staddr;
 var mainbutton = document.getElementById("submitmain");
 var straddrinput = document.getElementById("straddrinput");
+var srchpage = document.getElementById("searchpage");
+var respage = document.getElementById("resultspage");
 
 
-
+respage.style.display = "none";
 
 
 function getlonlat() {
@@ -11,7 +13,7 @@ function getlonlat() {
     // staddr = stradtemp.replace(/ /g, "%20");
     staddr = "84128";
     var apiurl = "http://dev.virtualearth.net/REST/v1/Locations/US/" + staddr + "?key=AgKBUuvbUwAsq1kz4VZEkPlVJZtYaLivsDp-uZ0w2DRIwUfOH-mQH5HFqPA0rpXe";
-    
+    mainbutton.classList.add("is-loading");
     fetch(apiurl, {
     })
         .then(function (response) {
@@ -19,16 +21,18 @@ function getlonlat() {
         })
         .then(function (data) {
             console.log(data);
-
-
-
+            
+            
             var lat = data.resourceSets[0].resources[0].point.coordinates[0];
             var lon = data.resourceSets[0].resources[0].point.coordinates[1];
             console.log(lat);
             console.log(lon);
 
+            mainbutton.classList.remove("is-loading");
+            srchpage.style.display = "none";
+            respage.style.display = "block";
 
-            
+
         });
 
 };
