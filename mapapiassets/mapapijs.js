f7ba1243da94076a887e9a2030be187ac29185fd
cassiegidd <124100826+cassiegidd@users.noreply.github.com>
2023-04-04 21:27:20 -0600

    nothing

--- mapapiassets/mapapijs.js
+++ mapapiassets/mapapijs.js
@@ -72,7 +72,7 @@ function getshops(latlon) {
     
 function getRoute() {    
 fetch("https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/Routes/Walking?waypoint.1=40.530622,-111.910037&waypoint.2=40.58739471,-111.93572998&pushpin=40.58739471,-111.93572998;59&maxSolutions=1&mapLayer=Basemap,buildings&format=jpeg&mapMetadata=0&key=AizrzYg48fADDG__bADnOBWOPofSFiBpuX2vBhjM6wV7JPPLXTj3il6kCztkuTo-")
-    .then((response)=>response);
+    .then((response)=>response.jpeg)
 }
 
 
